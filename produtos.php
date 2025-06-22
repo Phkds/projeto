6d37ce5582df4692006d4a58e9a6568cd8da0355
DoutoradosPaulo h kondazeski <paulokondazeski043@gmail.com>
2025-06-22 14:00:56 -0300

    ajeitando as camisas

--- produtos.php
+++ produtos.php
@@ -2,15 +2,15 @@
 // Array de produtos
 $produtos = [
     'times_europeus' => [
-        ['nome' => 'Camisa do Real Madrid', 'imagem' => 'imagens/real madrid.jpg', 'preco' => 'R$ 150,00'],
+        ['nome' => 'Camisa do Real Madrid', 'imagem' => 'imagens/real madri.jpg', 'preco' => 'R$ 150,00'],
         ['nome' => 'Camisa do Bayern de Munique', 'imagem' => 'imagens/bayer.jpg', 'preco' => 'R$ 140,00'],
         ['nome' => 'Camisa do Manchester United', 'imagem' => 'imagens/manchester u.jpg', 'preco' => 'R$ 160,00'],
         ['nome' => 'Camisa do Barcelona', 'imagem' => 'imagens/barça.jpg', 'preco' => 'R$ 155,00'],
         ['nome' => 'Camisa do Liverpool', 'imagem' => 'imagens/liverpool.jpg', 'preco' => 'R$ 145,00'],
         ['nome' => 'Camisa do Paris Saint-Germain', 'imagem' => 'imagens/psg.jpg', 'preco' => 'R$ 165,00'],
-        ['nome' => 'Camisa do Chelsea', 'imagem' => 'imagens/chelsea.jpg', 'preco' => 'R$ 150,00'],
+        ['nome' => 'Camisa do Borussia', 'imagem' => 'imagens/bvb.jpg', 'preco' => 'R$ 150,00'],
         ['nome' => 'Camisa do Juventus', 'imagem' => 'imagens/juventus.jpg', 'preco' => 'R$ 155,00'],
-        ['nome' => 'Camisa do Manchester City', 'imagem' => 'imagens/manchester_city.jpg', 'preco' => 'R$ 160,00'],
+        ['nome' => 'Camisa do Manchester Roma', 'imagem' => 'imagens/roma.jpg', 'preco' => 'R$ 160,00'],
         ['nome' => 'Camisa do Inter de Milão', 'imagem' => 'imagens/inter.jpg', 'preco' => 'R$ 150,00'],
         ['nome' => 'Camisa do Atlético de Madrid', 'imagem' => 'imagens/atletico de M.jpg', 'preco' => 'R$ 145,00']
     ],
@@ -21,19 +21,19 @@ $produtos = [
         ['nome' => 'Camisa do Santos', 'imagem' => 'imagens/santos.jpg', 'preco' => 'R$ 120,00'],
         ['nome' => 'Camisa do Corinthians', 'imagem' => 'imagens/corinthians.jpg', 'preco' => 'R$ 130,00'],
         ['nome' => 'Camisa do Grêmio', 'imagem' => 'imagens/gremio.jpg', 'preco' => 'R$ 125,00'],
-        ['nome' => 'Camisa do Internacional', 'imagem' => 'imagens/inter.jpg', 'preco' => 'R$ 135,00'],
+        ['nome' => 'Camisa do Internacional', 'imagem' => 'imagens/inter nacional.jpg', 'preco' => 'R$ 135,00'],
         ['nome' => 'Camisa do Atlético Mineiro', 'imagem' => 'imagens/atletico.jpg', 'preco' => 'R$ 130,00'],
         ['nome' => 'Camisa do Cruzeiro', 'imagem' => 'imagens/cruzeiro.jpg', 'preco' => 'R$ 125,00']
     ],
     'retro' => [
-        ['nome' => 'Camisa Retro 80s', 'imagem' => 'imagens/retro80s.jpg', 'preco' => 'R$ 100,00'],
-        ['nome' => 'Camisa Retro Anos 70', 'imagem' => 'imagens/retro70s.jpg', 'preco' => 'R$ 110,00'],
-        ['nome' => 'Camisa Retro Anos 90', 'imagem' => 'imagens/retro90s.jpg', 'preco' => 'R$ 120,00'],
-        ['nome' => 'Camisa Retro Anos 2000', 'imagem' => 'imagens/retro2000s.jpg', 'preco' => 'R$ 130,00'],
-        ['nome' => 'Camisa Retro Clássica', 'imagem' => 'imagens/retro_classica.jpg', 'preco' => 'R$ 140,00'],
-        ['nome' => 'Camisa Retro Vintage', 'imagem' => 'imagens/retro_vintage.jpg', 'preco' => 'R$ 150,00'],
-        ['nome' => 'Camisa Retro Estilo Anos 60', 'imagem' => 'imagens/retro60s.jpg', 'preco' => 'R$ 110,00'],
-        ['nome' => 'Camisa Retro Estilo Anos 50', 'imagem' => 'imagens/retro50s.jpg', 'preco' => 'R$ 120,00']
+        ['nome' => 'Camisa Retro 80s', 'imagem' => 'imagens/whest ham retro.jpg', 'preco' => 'R$ 100,00'],
+        ['nome' => 'Camisa Retro Anos 70', 'imagem' => 'imagens/retro real.jpg', 'preco' => 'R$ 110,00'],
+        ['nome' => 'Camisa Retro Anos 90', 'imagem' => 'imagens/milan retro.jpg', 'preco' => 'R$ 120,00'],
+        ['nome' => 'Camisa Retro Anos 2000', 'imagem' => 'imagens/manchester u retro.jpg', 'preco' => 'R$ 130,00'],
+        ['nome' => 'Camisa Retro Clássica', 'imagem' => 'imagens/frança retro.jpg', 'preco' => 'R$ 140,00'],
+        ['nome' => 'Camisa Retro Vintage', 'imagem' => 'imagens/barça retro.jpg', 'preco' => 'R$ 150,00'],
+        ['nome' => 'Camisa Retro Estilo Anos 60', 'imagem' => 'imagens/arsenal retro.jpg', 'preco' => 'R$ 110,00'],
+        ['nome' => 'Camisa Retro Estilo Anos 50', 'imagem' => 'imagens/retro.jpg', 'preco' => 'R$ 120,00']
     ]
 ];
 ?>
