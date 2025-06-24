b84ccb00e6e20cdaf48199453583222f21d1dba1
DoutoradosPaulo h kondazeski <paulokondazeski043@gmail.com>
2025-06-23 23:03:35 -0300

    limpo

--- index-limpo.php
+++ index-limpo.php
@@ -1,15 +1,9 @@
-<?php
-
-$base = "http://{ $_SERVER['SERVER_NAME'] }{ $_SERVER['SERVE_NAME'] }";
-?>
-
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
   <title>Vitoriano Sports</title>
-  <base href="<?=base ?>">
 
   <!-- Bootstrap CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous" />
